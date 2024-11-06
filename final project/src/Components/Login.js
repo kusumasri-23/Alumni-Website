4990c5c13d8b48a4ecd6aa46fa71f8f5474555a8
kusumasri-23 <161683093+kusumasri-23@users.noreply.github.com>
2024-11-06 20:00:31 +0530

    Update Login.js

--- final project/src/Components/Login.js
+++ final project/src/Components/Login.js
@@ -1,1 +1,38 @@
-
+import './styles.css';
+function Login(){
+    return(
+        <div class="wrapper">
+        <div class="form-holder">
+            <h2>Forgot Password?</h2>
+            <form className="form" onSubmit="http://localhost:3000/register/save" method="post">
+                <div className="form-group">
+                        <img src="/images/email.png" alt="Email Icon" />
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            
+                            required
+                        />
+                </div>
+                <div className="form-group">
+                        <img src="/images/lock.png" alt="Lock Icon" />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            
+                            required
+                        />
+                    </div>
+                <div className="form-group">
+                        <button type="submit" >Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    );
+}
+export default Login;
