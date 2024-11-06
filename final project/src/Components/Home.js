a09082e9a6c70c94ffae77324795ef20c040a2ca
kusumasri-23 <161683093+kusumasri-23@users.noreply.github.com>
2024-11-06 20:03:49 +0530

    Update Home.js

--- final project/src/Components/Home.js
+++ final project/src/Components/Home.js
@@ -1,1 +1,16 @@
+import './styles.css'; 
+function Home(){
+    return(
+        <div>
+        <div class="hero">
+            <img src='home.jpg'/>
+        <div class="hero-content">
+            <h1>Welcome to the Alumni Management System</h1>
+            <p>Connecting alumni for collaboration and networking.</p>
+        </div>
+        </div>
+        </div>
 
+    );
+}
+export default Home;
