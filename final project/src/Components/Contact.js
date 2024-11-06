db93995738f0e7b90dcbe8255964809187152dd9
kusumasri-23 <161683093+kusumasri-23@users.noreply.github.com>
2024-11-06 20:13:16 +0530

    Create Contact.js

---
+++ final project/src/Components/Contact.js
@@ -0,0 +1,11 @@
+import './Contact.css';
+function Contact(){
+    return(
+        <div class='container'>
+            <h1>This is Contact Page</h1>
+            <h1>Contact Number : 9876543210</h1>
+            <h1>Email : abc@gmail.com</h1>
+        </div>
+    );
+}
+export default Contact;
