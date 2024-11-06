667f6d99b65345c5c4f9683189b4a8f1fe296265
kusumasri-23 <161683093+kusumasri-23@users.noreply.github.com>
2024-11-06 20:12:28 +0530

    Create About.js

---
+++ final project/src/Components/About.js
@@ -0,0 +1,19 @@
+import './About.css';
+function About(){
+    return(
+        <div>
+            <div className="heading">
+            <h1>ABOUT US</h1>
+        </div>
+        <div className="container">
+            <img src="/images/WT5.jpeg"/>
+            <img src="/images/WT6.jpeg" width="400px"/>
+            <img src="/images/WT7.jpeg" width="400px"/>
+            <img src="/images/WT8.jpeg" width="300px"/>
+            <img src="/images/WT9.jpeg" width="300px" height="300"/>
+            <img src="/images/WT10.jpeg" width="300px"/>
+        </div>
+        </div>       
+    );
+}
+export default About;
