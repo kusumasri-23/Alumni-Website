3c4897a0a8aa20c9525a4c408941a8b0b7ef8a57
kusumasri-23 <161683093+kusumasri-23@users.noreply.github.com>
2024-11-06 19:49:09 +0530

    Update app.js

--- final project/src/app.js
+++ final project/src/app.js
@@ -1,1 +1,26 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from './components/Home';
+import NewsRoom from './components/NewsRoom';
+import About from './components/About';
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Register from "./components/Register";
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/newsroom" Component={NewsRoom}/>
+        <Route path="/about" Component={About}/>
+        <Route path="/contact" Component={Contact}/>
+        <Route path="/login" Component={Login}/>
+        <Route path="/register" Component={Register}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
