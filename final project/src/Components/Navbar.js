3e132517e993fb30824456082e9ff22c6ea05f42
kusumasri-23 <161683093+kusumasri-23@users.noreply.github.com>
2024-11-06 20:04:49 +0530

    Update Navbar.js

--- final project/src/Components/Navbar.js
+++ final project/src/Components/Navbar.js
@@ -1,1 +1,29 @@
-
+import {Link} from 'react-router-dom';
+import "./Navbar.css";
+function Navbar(){
+    return(
+        <div class='navbar'>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/newsroom'>NewsRoom</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+                <li>
+                    <Link to='/login'>Login</Link>
+                </li>
+                <li>
+                    <Link to='/register'>Register</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+export default Navbar;
